3445edf9ff98864bb36ea8244945d65f2883468f
aravindhsharma <49915185+aravindhsharma@users.noreply.github.com>
2021-12-06 09:40:50 +0530

    Update Google Signin

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,11 @@ export default NextAuth({
     Providers.Facebook({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    }),    
+    }),
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     // ...add more providers here
   ],
 });
